de071c22faad6a346fd8741cce10024d389aa760
msegnit <melsegnit@gmail.com>
2020-02-24 15:50:50 -0600

    add delete skill

--- routes/skills.js
+++ routes/skills.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var skillDb = require('../models/skill')
 var skillsCtrl = require('../controllers/skills')
 
 router.get('/', skillsCtrl.index);
